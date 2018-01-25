3209fde6aa341fa76f86279ba4505060f920bd3c
Steve Cardone <cardonesc@yahoo.com>
2018-01-25 15:50:11 -0600

    Worked on Gravity Physics

--- SpacePhysics/src/spacePhys1/Object.java
+++ SpacePhysics/src/spacePhys1/Object.java
@@ -4,36 +4,88 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class Object {
-
+	//MOVEMENT VARS
 	// xLoc and yLoc will be calculated by move() and set in constructor
 	double xLoc;
 	double yLoc;
-	
-	//This is my momentum vector
-	double vectorTheta;
-	double vectorMagnitude;
+
+	//OTHER VARS
+	double mass;
 	
 	public Object() {
 		
 	}
 	
-	public Object(double xLoc,double yLoc,double vectorTheta,double vectorMagnitude) {
+	public Object(double xLoc,double yLoc, double mass) {
 		this.xLoc = xLoc;
 		this.yLoc = yLoc;
-		this.vectorTheta = vectorTheta;
-		this.vectorMagnitude = vectorMagnitude;
+		this.mass = mass;
 	}
 	
 	
+	public double getxLoc() {
+		return xLoc;
+	}
+
+	public void setxLoc(double xLoc) {
+		this.xLoc = xLoc;
+	}
+
+	public double getyLoc() {
+		return yLoc;
+	}
+
+	public void setyLoc(double yLoc) {
+		this.yLoc = yLoc;
+	}
+
+	public double getMass() {
+		return mass;
+	}
+
+	public void setMass(double mass) {
+		this.mass = mass;
+	}
+
 	public void paintObject(Graphics g){
 		
 		g.setColor(Color.WHITE);
-		g.fillOval((int)this.xLoc,(int)this.yLoc,20,20);
+		g.fillOval((int)this.xLoc,(int)-this.yLoc,20,20);
+		
 		
 	}
 	
-	
-	public void move(int centerX, int centerY) {
-		this.yLoc++;
+	// Movement methods
+	public void move(double gravMag, double gravDir) {
+		
+		this.xLoc += (gravMag * Math.cos(gravDir));
+		this.yLoc += (gravMag * Math.sin(gravDir));
 	}
+	
+	public double distanceBetween(Object a, Object b){
+		
+		return Math.sqrt(  (a.getxLoc() + b.getxLoc())*(a.getxLoc() + b.getxLoc()) 
+				+ (a.getyLoc() + b.getyLoc())*(a.getyLoc() + b.getyLoc()));
+	}
+	
+	public static double gravEquasion(Object a, Object b, double r){
+		double force;
+		
+		force = ( 9.8*(a.getMass()*b.getMass()) / (r*r) );
+		return force;
+	}
+	
+	public static double gravDirection(Object a, Object b){
+		double degrees;
+		double cXLoc;
+		double cYLoc;
+		
+		cXLoc = b.getxLoc() - a.getxLoc();
+		cYLoc = b.getyLoc() - a.getyLoc();
+		
+		degrees = Math.toDegrees(Math.atan(cXLoc/cYLoc));
+		
+		return degrees;
+	}
+
 }
--- SpacePhysics/src/spacePhys1/System.java
+++ SpacePhysics/src/spacePhys1/System.java
@@ -3,11 +3,13 @@ package spacePhys1;
 import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.util.ArrayList;
+
 
 @SuppressWarnings("serial")
 public class System extends Applet implements Runnable {
-	int width = 3840, height = 2160;
+	int width = 1200, height = 900;
 	int numberOfObjects = 300;
 	
 	Thread thread;
@@ -25,36 +27,38 @@ public class System extends Applet implements Runnable {
 		setBackground (Color.black);
 		g.translate(width/2, height/2);
 		
-		for(Object x : objects) {
-			x.paintObject(g);
+		
+		for(Object a : objects) {
+			a.paintObject(g);
 		}
 		
 }
 
 	
 	public void update() {
-		//calculate the weighted center point of gravity
-		int centerX = 0;
-		int centerY = 0;
-		//apply the acceleratoin to each object toward the central gravity point and move
-		for(Object x : objects) {
-			x.move(centerX, centerY);
-		}
+
+		moveObjs();
+
 	}
 
 	public void run() {		
 		
 //here is where you make your initial materials with a for loop}
-		//Object sun = new Object(0,0,0,0);		
-		//objects.add(sun);
 		
-		Object testObj = new Object(300,300,20,20);
-		objects.add(testObj);
+		Object sun = new Object(0,0,1000);		
+		objects.add(sun);
 		
-		for(int x = 2 ; x < numberOfObjects; x++) {
-			//Use this space to create my list of objects
-			
-		}
+		Object sun2 = new Object(100,100,10);		
+		objects.add(sun2);
+		
+//		Object sun3 = new Object(100,200,500);		
+//		objects.add(sun3);
+		
+//		for(int x = 2 ; x < numberOfObjects; x++) {
+//			Object tempObj = new Object();
+//			
+//			objects.add(tempObj);
+//		}
 		
 		for(;;){
 			//This is what loops the system to simulate motion
@@ -63,11 +67,37 @@ public class System extends Applet implements Runnable {
 
 			try {
 				//Change sleep time to speed or slow simulation.
-				Thread.sleep(10);
+				Thread.sleep(1000);
 			} catch (InterruptedException e) {
 				
 				e.printStackTrace();
 			}
+			
+		}
+		
+	}
+	
+	private void moveObjs(){
+		double gravityMagnitude = 0;
+		double gravityDirection = 0;
+		double distBetween;
+		
+		for(Object a : objects) {
+
+			for(Object b : objects){
+				if(a == b){
+					continue;
+				}
+				double these2Grav;
+				double these2Dir;
+				distBetween = a.distanceBetween(a, b);
+				these2Grav = Object.gravEquasion(a,b,distBetween);
+				these2Dir = Object.gravDirection(a,b);
+				gravityMagnitude += these2Grav;
+				gravityDirection += these2Dir;
+			}
+			
+			a.move(gravityMagnitude, gravityDirection);
 			
 		}
 		
